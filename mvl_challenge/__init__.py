c9fb6c7a1602690ac39e736bd9b3c0319262297d
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2023-03-06 08:15:27 +0800

    Minor change

--- mvl_challenge/__init__.py
+++ mvl_challenge/__init__.py
@@ -1,7 +1,7 @@
 import os 
 
-mvl_challenge_DIR = os.path.dirname(__file__)
-CFG_DIR = os.path.join(mvl_challenge_DIR, "data/configs")
-ASSETS_DIR = os.path.join(mvl_challenge_DIR, 'assets')
+ROOT_DIR = os.path.dirname(__file__)
+CFG_DIR = os.path.join(ROOT_DIR, "data/configs")
+ASSETS_DIR = os.path.join(ROOT_DIR, 'assets')
 
 EPILOG = "\t * MVL-Challenge - CVPR 2023 - OmniCV workshop"
