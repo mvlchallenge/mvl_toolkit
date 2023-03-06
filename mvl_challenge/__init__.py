295ca1e112d919ce3125343ff6a562ad7588a0d5
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2023-03-06 11:28:01 +0800

    add DATA_DIR to default PATHS

--- mvl_challenge/__init__.py
+++ mvl_challenge/__init__.py
@@ -3,5 +3,6 @@ import os
 ROOT_DIR = os.path.dirname(__file__)
 CFG_DIR = os.path.join(ROOT_DIR, "data/configs")
 ASSETS_DIR = os.path.join(ROOT_DIR, 'assets')
+DATA_DIR = os.path.join(ROOT_DIR, 'data')
 
 EPILOG = "\t * MVL-Challenge - CVPR 2023 - OmniCV workshop"
