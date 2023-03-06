386e23fa4be62291091102649b519bf477795a3c
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2023-03-06 19:05:47 +0800

    Add global OUTPUT_DIR to run file

--- mvl_challenge/pre_processing/pre_process_rgbd_data.sh
+++ mvl_challenge/pre_processing/pre_process_rgbd_data.sh
@@ -1,49 +1,49 @@
 #!/bin/sh
-
+OUTPUT_DIR=/media/public_dataset/mvl_challenge
 # ! scene_room_idx files 
 python mvl_challenge/pre_processing/create_scene_room_idx_list.py \
         -d /media/public_dataset/HM3D-MVL/test/ \
         -f hm3d_mvl__scene_test_list \
-        -o /media/public_dataset/mvl_challenge/ -m 5
+        -o "$OUTPUT_DIR"/ -m 5
 
 python mvl_challenge/pre_processing/create_scene_room_idx_list.py \
         -d /media/public_dataset/MP3D_360_FPE/SINGLE_ROOM_SCENES/ \
         -f mp3d_fpe__single_room_scene_list \
-        -o /media/public_dataset/mvl_challenge/ -m 5
+        -o "$OUTPUT_DIR"/ -m 5
 
 python mvl_challenge/pre_processing/create_scene_room_idx_list.py \
         -d /media/public_dataset/MP3D_360_FPE/MULTI_ROOM_SCENES/ \
         -f mp3d_fpe__multi_room_scene_list \
-        -o /media/public_dataset/mvl_challenge/ -m 5
+        -o "$OUTPUT_DIR"/ -m 5
 
 # # ! geometry info files
-# python mvl_challenge/pre_processing/create_geometry_info_files.py \
-#         -d /media/public_dataset/HM3D-MVL/test/ \
-#         --scene_list /media/public_dataset/mvl_challenge/hm3d_mvl__scene_test_list.json \
-#         -o /media/public_dataset/mvl_challenge/hm3d_mvl/geometry_info
+python mvl_challenge/pre_processing/create_geometry_info_files.py \
+        -d /media/public_dataset/HM3D-MVL/test/ \
+        --scene_list "$OUTPUT_DIR"/hm3d_mvl__scene_test_list.json \
+        -o "$OUTPUT_DIR"/hm3d_mvl/geometry_info
 
-# python mvl_challenge/pre_processing/create_geometry_info_files.py \
-#         -d /media/public_dataset/MP3D_360_FPE/SINGLE_ROOM_SCENES/ \
-#         --scene_list /media/public_dataset/mvl_challenge/mp3d_fpe__single_room_scene_list.json \
-#         -o /media/public_dataset/mvl_challenge/mp3d_fpe/geometry_info
+python mvl_challenge/pre_processing/create_geometry_info_files.py \
+        -d /media/public_dataset/MP3D_360_FPE/SINGLE_ROOM_SCENES/ \
+        --scene_list "$OUTPUT_DIR"/mp3d_fpe__single_room_scene_list.json \
+        -o "$OUTPUT_DIR"/mp3d_fpe/geometry_info
 
-# python mvl_challenge/pre_processing/create_geometry_info_files.py \
-#         -d /media/public_dataset/MP3D_360_FPE/MULTI_ROOM_SCENES/ \
-#         --scene_list /media/public_dataset/mvl_challenge/mp3d_fpe__multi_room_scene_list.json \
-#         -o /media/public_dataset/mvl_challenge/mp3d_fpe/geometry_info
+python mvl_challenge/pre_processing/create_geometry_info_files.py \
+        -d /media/public_dataset/MP3D_360_FPE/MULTI_ROOM_SCENES/ \
+        --scene_list "$OUTPUT_DIR"/mp3d_fpe__multi_room_scene_list.json \
+        -o "$OUTPUT_DIR"/mp3d_fpe/geometry_info
 
-# # ! RGB files
-# python mvl_challenge/pre_processing/create_rgb_files.py \
-#         -d /media/public_dataset/HM3D-MVL/test/ \
-#         -g /media/public_dataset/mvl_challenge/hm3d_mvl/geometry_info \
-#         -o /media/public_dataset/mvl_challenge/hm3d_mvl/img
+# ! RGB files
+python mvl_challenge/pre_processing/create_rgb_files.py \
+        -d /media/public_dataset/HM3D-MVL/test/ \
+        -g "$OUTPUT_DIR"/hm3d_mvl/geometry_info \
+        -o "$OUTPUT_DIR"/hm3d_mvl/img
 
-# python mvl_challenge/pre_processing/create_rgb_files.py \
-#         -d /media/public_dataset/MP3D_360_FPE/SINGLE_ROOM_SCENES/ \
-#         -g /media/public_dataset/mvl_challenge/mp3d_fpe/geometry_info \
-#         -o /media/public_dataset/mvl_challenge/mp3d_fpe/img
+python mvl_challenge/pre_processing/create_rgb_files.py \
+        -d /media/public_dataset/MP3D_360_FPE/SINGLE_ROOM_SCENES/ \
+        -g "$OUTPUT_DIR"/mp3d_fpe/geometry_info \
+        -o "$OUTPUT_DIR"/mp3d_fpe/img
 
-# python mvl_challenge/pre_processing/create_rgb_files.py \
-#         -d /media/public_dataset/MP3D_360_FPE/MULTI_ROOM_SCENES/ \
-#         -g /media/public_dataset/mvl_challenge/mp3d_fpe/geometry_info \
-#         -o /media/public_dataset/mvl_challenge/mp3d_fpe/img
+python mvl_challenge/pre_processing/create_rgb_files.py \
+        -d /media/public_dataset/MP3D_360_FPE/MULTI_ROOM_SCENES/ \
+        -g "$OUTPUT_DIR"/mp3d_fpe/geometry_info \
+        -o "$OUTPUT_DIR"/mp3d_fpe/img
